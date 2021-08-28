74cecdf044142f60726a646779b4123c87f7f1ff
Ritish jain <mynameisritish@gmail.com>
2021-08-28 17:10:19 +0530

    Subscribe button added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,9 @@
 export default function Contact() {
     return (
         <div className="container">
-            <h1>Contact us</h1>
+            <h1 className="my-4">Contact us</h1>
+
+            <a href="https://www.youtube.com/channel/UCHiBsgw-EuyyS2J8j3NEFPg" className="text-decoration-none"><ul><strong className="text-dark">Our YouTube Channel:</strong><button  type="button" className="btn btn-danger mx-2">Subscribe</button></ul> </a>
             <ul><strong>Phone Number: </strong>6280936040</ul>
             <ul><strong>Instagram id: </strong>@aruma_films</ul>
             <ul><strong>Email id: </strong>mynameisritish@gmail.com</ul>
