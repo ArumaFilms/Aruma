ce929700faed73233ce14b15c938dc6b522b86ad
Ritish jain <mynameisritish@gmail.com>
2021-08-28 16:38:08 +0530

    master ri

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
 import './App.css';
+import React,{useState} from 'react'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import Contact from './Components/Contact'
+import TextUtilies from './Components/TextUtilies';
+import Alert from './Components/Alert';
 
-import React, { Component } from 'react'
-import NavBar from './Component/NavBar';
-import News from './Component/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar/>
-        <News/>
-      </div>
-    )
-  }
+function App() {
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type)=>{
+    setAlert({
+        msg: message,
+        type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+}
+    
+  return (
+    <>
+      <Router>
+        <Navbar title="Aruma Films"/>
+        <Alert  alert= {alert}/>
+        <div className="container">
+          <Switch>
+              <Route exact path="/Home">
+                <Home/>
+              </Route>
+              <Route exact path="/">
+                <Home/>
+              </Route>
+              <Route exact path="/Aruma">
+                <Home/>
+              </Route>
+              <Route exact path="/TextUtilies">
+                <TextUtilies heading="Enter the text" showAlert={showAlert}/>
+              </Route>
+              <Route path="/Contact">
+                <Contact/>
+              </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
 }
 
+export default App;
