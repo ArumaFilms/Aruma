75df31c060e507d67379dfbf550c775d372c0d32
Ritish jain <mynameisritish@gmail.com>
2021-08-29 12:29:46 +0530

    Home button to Aruma path

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(props) {
         <>
         <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
         <div className="container-fluid">
-        <Link className="navbar-brand" to="/Home">
+        <Link className="navbar-brand" to="/Aruma">
         {props.title}
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,7 +18,7 @@ export default function Navbar(props) {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             {/* nav ka items */}
             <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/Home">Home</Link>
+              <Link className="nav-link" aria-current="page" to="/Aruma">Home</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/TextUtilies">TextUtilies</Link>
